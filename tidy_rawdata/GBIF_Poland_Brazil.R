70d53a248bdab1830eeca4c5f994948e025f0b62
Leonardo Kamogawa <leonardo.kamogawa@hotmail.com>
2026-01-15 01:09:02 -0300

    feat: added Brazil in data

--- tidy_rawdata/GBIF_Poland_Brazil.R
+++ tidy_rawdata/GBIF_Poland_Brazil.R
@@ -6,29 +6,29 @@ View(read_csv("~/Downloads/biodiversity-data/occurence.csv", n_max = 10000))
 View(read_csv("~/Downloads/biodiversity-data/multimedia.csv", n_max = 10000))
 
 # Now, we use readr::read_csv_chunk to handle this large occurence.csv file and use the `callback`
-# argument to filter just the rows where `country == "Poland"`.
-occurence_filter <- function(chunk, pos) {
-  chunk |> filter(country == "Poland")
+# argument to filter just the rows where `country %in% c("Poland", "Brazil")`.
+occurrence_filter <- function(chunk, pos) {
+  chunk |> filter(country %in% c("Poland", "Brazil"))
 }
 
-occurence_poland <- read_csv_chunked(
+occurrence <- read_csv_chunked(
   "~/Downloads/biodiversity-data/occurence.csv",
-  callback = DataFrameCallback$new(occurence_filter)
+  callback = DataFrameCallback$new(occurrence_filter)
 )
 
-# With the `ids` from `occurence_poland`, we can filter the rows in multimedia.csv where
-# `CoreId %in% `occurence_poland$id`.
+# With the `ids` from `occurrence_poland`, we can filter the rows in multimedia.csv where
+# `CoreId %in% `occurrence$id`.
 multimedia_filter <- function(chunk, pos) {
-  chunk |> filter(CoreId %in% occurence_poland$id)
+  chunk |> filter(CoreId %in% occurrence$id)
 }
 
-multimedia_poland <- read_csv_chunked(
+multimedia <- read_csv_chunked(
   "~/Downloads/biodiversity-data/multimedia.csv",
   callback = DataFrameCallback$new(multimedia_filter)
 )
 
 # Finally, transform the data frames to .rda files to call in the app.
-usethis::use_data(occurence_poland, overwrite = TRUE)
-usethis::use_data(multimedia_poland, overwrite = TRUE)
+usethis::use_data(occurrence, overwrite = TRUE)
+usethis::use_data(multimedia, overwrite = TRUE)
 
 
