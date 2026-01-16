b76ca93e944ff3bbf51cec68ba2271f9532a81b1
Leonardo Kamogawa <leonardo.kamogawa@hotmail.com>
2026-01-15 23:40:31 -0300

    build: All packages on Depends because of deploy error

--- R/run_app.R
+++ R/run_app.R
@@ -1,7 +1,5 @@
 run_app <- function() {
   shiny::addResourcePath("dir", system.file(package = "ShinyGBIF", "www"))
 
-  options(sass.cache = FALSE)
-
   shiny::shinyApp(ui = ui(), server = server, option = list(launch.browser = TRUE))
 }
