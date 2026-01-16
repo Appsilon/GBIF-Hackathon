bcc53b2aaba883fb6c79c71a79bf2571ecf12ce9
Leonardo Kamogawa <leonardo.kamogawa@hotmail.com>
2026-01-15 23:33:57 -0300

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,7 @@
 run_app <- function() {
   shiny::addResourcePath("dir", system.file(package = "ShinyGBIF", "www"))
 
+  options(sass.cache = FALSE)
+
   shiny::shinyApp(ui = ui(), server = server, option = list(launch.browser = TRUE))
 }
